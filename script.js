1a51d42deea65f310f482e8439540525ca14513a
tlacolley <thomaslacolley@gmail.com>
2018-06-14 15:53:58 +0200

    creation mvt random de la div

--- script.js
+++ script.js
@@ -1,11 +1,25 @@
-var target = document.getElementById("target");
+// var target = document.getElementById("red");
+// var positionX = target.offsetTop;
+// var positionY = target.offsetLeft;
+
+console.log("positionX");
+var target = document.getElementById("red");
+console.log(target);
+target.style.transitionDuration = "3s"
 function deplacement () {
-    target.style.top +=5 +"px";
-    target.style.left += 5 +"px";
-    // var Xposition =  target.style.top;
-    // var Yposition = target.style.left;
-    // Xposition = +5;
+
+    var positionX = target.offsetBottom;
+    var positionY = target.offsetLeft;
+    var newX = Math.random()*1000;
+    var newY = Math.random()*1000;
+
+    target.style.top = newX + "px";
+    console.log(newX);
+    target.style.left = newY + "px";
 };
-var z = target.offsetTop;
-console.log(z);
-// var move = window.setInterval(deplacement, 1000);
+// var z = target.offsetTop;
+// console.log(target.offsetTop);
+
+
+
+var move = window.setInterval(deplacement, 3000);
