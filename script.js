0da86f164e21e564a77df68634033cac7e33015e
gosset <dadgosset@orange.f>
2018-06-14 16:47:46 +0200

    ajout de plusieurs cible en html et css

--- script.js
+++ script.js
@@ -1,25 +1,23 @@
-// var target = document.getElementById("red");
-// var positionX = target.offsetTop;
-// var positionY = target.offsetLeft;
+
 
 console.log("positionX");
-var target = document.getElementById("red");
-console.log(target);
-target.style.transitionDuration = "3s"
+var targets = document.getElementsByClassName("target");
+console.log(targets);
+
+
 function deplacement () {
 
-    var positionX = target.offsetBottom;
-    var positionY = target.offsetLeft;
+    for (var i = 0; i < targets.length; i++) {
+
+    var positionX = this.offsetTop;
+    var positionY = this.offsetLeft;
     var newX = Math.random()*1000;
     var newY = Math.random()*1000;
 
-    target.style.top = newX + "px";
+    targets[i].style.top = newX + "px";
     console.log(newX);
-    target.style.left = newY + "px";
+    targets[i].style.left = newY + "px";
+    targets[i].style.transitionDuration = "3s";
+    }
 };
-// var z = target.offsetTop;
-// console.log(target.offsetTop);
-
-
-
-var move = window.setInterval(deplacement, 3000);
+var move = window.setInterval(deplacement, 1000);
