c4925f1d89e34a0ef8d287c268e110c9156f188a
gosset <dadgosset@orange.f>
2018-06-14 14:55:06 +0200

    html css fait et debut du codage js

---
+++ script.js
@@ -0,0 +1,11 @@
+var target = document.getElementById("target");
+function deplacement () {
+    target.style.top +=5 +"px";
+    target.style.left += 5 +"px";
+    // var Xposition =  target.style.top;
+    // var Yposition = target.style.left;
+    // Xposition = +5;
+};
+var z = target.offsetTop;
+console.log(z);
+// var move = window.setInterval(deplacement, 1000);
